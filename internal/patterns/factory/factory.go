d2e24e550028771e8d93515984beb07a4dd74496
shu.yamaga <s.yamaga.0318@gmail.com>
2025-02-23 13:55:30 +0000

    [remove]#facroty_change reffrerence of DataProcessor

--- internal/patterns/factory/factory.go
+++ internal/patterns/factory/factory.go
@@ -1,16 +1,19 @@
 package factory
 
-import "github.com/sh-yamaga/design-patterns-go/internal/patterns/factory/processor"
+import "github.com/sh-yamaga/design-patterns-go/internal/patterns/template"
 
 // DataProcessorFactory defines the factory method for creating IDataProcessor instances
 type DataProcessorFactory interface {
-	CreateCsvProcessor() processor.IDataProcessor
+	CreateCsvProcessor() template.IDataProcessor
 }
 
 // ProcessorFactory is a concrete factory for creating CsvProcessor instances
 type ProcessorFactory struct{}
 
 // CreateCsvProcessor creates a new CsvProcessor instance
-func (f *ProcessorFactory) CreateCsvProcessor() processor.IDataProcessor {
-	return processor.NewCsvProcessor()
+func (f *ProcessorFactory) CreateCsvProcessor() template.IDataProcessor {
+	cp := &template.CsvProcessor{}
+	cp.BaseProcessor.IDataProcessor = cp
+
+	return cp
 }
