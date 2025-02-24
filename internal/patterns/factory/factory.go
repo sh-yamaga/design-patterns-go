0bc70e794372c02efa1028b3045518153e33ac9f
shu.yamaga <s.yamaga.0318@gmail.com>
2025-02-24 04:06:48 +0000

    [update]#factory_fix factory interface return type

--- internal/patterns/factory/factory.go
+++ internal/patterns/factory/factory.go
@@ -4,14 +4,14 @@ import "github.com/sh-yamaga/design-patterns-go/internal/patterns/template"
 
 // DataProcessorFactory defines the factory method for creating IDataProcessor instances
 type DataProcessorFactory interface {
-	CreateCsvProcessor() template.IDataProcessor
+	CreateCsvProcessor() *template.CsvProcessor
 }
 
 // ProcessorFactory is a concrete factory for creating CsvProcessor instances
 type ProcessorFactory struct{}
 
 // CreateCsvProcessor creates a new CsvProcessor instance
-func (f *ProcessorFactory) CreateCsvProcessor() template.IDataProcessor {
+func (f *ProcessorFactory) CreateCsvProcessor() *template.CsvProcessor {
 	cp := &template.CsvProcessor{}
 	cp.BaseProcessor.IDataProcessor = cp
 
