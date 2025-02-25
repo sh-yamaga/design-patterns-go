e0738d7ac850380c43907fb70caf9b43270a6a95
shu.yamaga <s.yamaga.0318@gmail.com>
2025-02-25 00:35:41 +0000

    [update]#factory_update return value's type. from concrete struct to IDataProcessor

--- internal/patterns/factory/factory.go
+++ internal/patterns/factory/factory.go
@@ -6,23 +6,23 @@ import (
 
 // DataProcessorFactory defines the factory method for creating IDataProcessor instances
 type DataProcessorFactory interface {
-	NewCsvProcessor() *processor.CsvProcessor
-	NewXmlProcessor() *processor.XmlProcessor
+	NewCsvProcessor() *processor.IDataProcessor
+	NewXmlProcessor() *processor.IDataProcessor
 }
 
 // ProcessorFactory is a concrete factory for creating CsvProcessor instances
 type ProcessorFactory struct{}
 
-// NewCsvProcessor creates a new CsvProcessor instance
-func (pf *ProcessorFactory) NewCsvProcessor() *processor.CsvProcessor {
+// NewCsvProcessor creates a new CsvProcessor instance, implements IDataProcessor
+func (pf *ProcessorFactory) NewCsvProcessor() processor.IDataProcessor {
 	cp := &processor.CsvProcessor{}
 	cp.BaseProcessor.IDataProcessor = cp
 
 	return cp
 }
 
-// NewXmlProcessor creates a new XmlProcessor instance
-func (pf *ProcessorFactory) NewXmlProcessor() *processor.XmlProcessor {
+// NewXmlProcessor creates a new XmlProcessor instance, implements IDataProcessor
+func (pf *ProcessorFactory) NewXmlProcessor() processor.IDataProcessor {
 	xp := &processor.XmlProcessor{}
 	xp.BaseProcessor.IDataProcessor = xp
 
