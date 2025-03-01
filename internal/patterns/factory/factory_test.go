eae8e46dfe99c4c34129b21f034bdf5287a9fbf4
shu.yamaga <s.yamaga.0318@gmail.com>
2025-03-01 02:14:43 +0000

    [update]#factory_update factory test

--- internal/patterns/factory/factory_test.go
+++ internal/patterns/factory/factory_test.go
@@ -2,23 +2,20 @@ package factory
 
 import (
 	"testing"
-
-	"github.com/sh-yamaga/design-patterns-go/internal/patterns/factory/book"
-	"github.com/sh-yamaga/design-patterns-go/internal/patterns/factory/movie"
 )
 
 func TestWorkFactory(t *testing.T) {
 	var wf WorkFactory = WorkFactory{}
-	var bf book.BookFactory = wf.BookFactory()
-	var mf movie.MovieFactory = wf.MovieFactory()
+	var bookFactory WorkFactory = wf.Create(Book)
+	var movieFactory WorkFactory = wf.Create(Movie)
 
-	// Check if bf is of the expected type
-	if _, ok := interface{}(bf).(book.BookFactory); !ok {
-		t.Errorf("Expected bf to be of type book.BookFactory, got %T", bf)
+	// Check if bookFactory is of the expected type
+	if _, ok := interface{}(bookFactory).(WorkFactory); !ok {
+		t.Errorf("Expected bookFactory to be of type WorkFactory, got %T", bookFactory)
 	}
 
-	// Check if mf is of the expected type
-	if _, ok := interface{}(mf).(movie.MovieFactory); !ok {
-		t.Errorf("Expected mf to be of type movie.MovieFactory, got %T", mf)
+	// Check if movieFactory is of the expected type
+	if _, ok := interface{}(movieFactory).(WorkFactory); !ok {
+		t.Errorf("Expected movieFactory to be of type WorkFactory, got %T", movieFactory)
 	}
 }
