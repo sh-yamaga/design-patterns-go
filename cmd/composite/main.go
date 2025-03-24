420650fa4557490c0b3bd9e15aa7de3882adc88d
shu.yamaga <s.yamaga.0318@gmail.com>
2025-03-24 10:49:09 +0000

    [update]#composite_create client image

--- cmd/composite/main.go
+++ cmd/composite/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+type Area interface {
+	GetPopulation() uint
+}
+
+func main() {
+	jp := NewCountry()
+	jp.AddPrefecture(&Prefecture{
+		name:       "tokyo",
+		population: 14_200_331,
+	})
+	jp.AddPrefecture(&Prefecture{
+		name:       "hokkaido",
+		population: 5_039_100,
+	})
+
+	fmt.Println(jp.GetPopulation())
 }
