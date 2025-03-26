1c8e67698a88921b0d16cd6284137ae04d031c5d
shu.yamaga <s.yamaga.0318@gmail.com>
2025-03-26 11:33:35 +0000

    [update]#composite_create client code sample

--- cmd/composite/main.go
+++ cmd/composite/main.go
@@ -1,21 +1,39 @@
 package main
 
-import "fmt"
-
-type Area interface {
-	GetPopulation() uint
-}
+import "github.com/sh-yamaga/design-patterns-go/internal/patterns/composite"
 
 func main() {
-	jp := NewCountry()
-	jp.AddPrefecture(&Prefecture{
-		name:       "tokyo",
-		population: 14_200_331,
-	})
-	jp.AddPrefecture(&Prefecture{
-		name:       "hokkaido",
-		population: 5_039_100,
-	})
+	// create root dir
+	root := &composite.Directory{Name: "root", Level: 0}
 
-	fmt.Println(jp.GetPopulation())
+	// create child dir
+	dir2 := &composite.Directory{Name: "dir2"}
+	dir1 := &composite.Directory{Name: "dir1"}
+
+	// create files
+	file1 := &composite.File{Name: "file1.txt"}
+	file2 := &composite.File{Name: "file2.txt"}
+	file3 := &composite.File{Name: "file3.txt"}
+
+	// construct
+	// root -> dir2 -> (dir1 -> (file1, file2), file3)
+
+	root.AddDir(dir2)
+
+	dir2.AddDir(dir1)
+
+	dir1.AddFile(file1)
+	dir1.AddFile(file2)
+
+	dir2.AddFile(file3)
+
+	// show tree
+	root.Show()
+	// Output:
+	// root
+	//   dir2
+	//     dir1
+	//       file1.txt
+	//       file2.txt
+	//     file3.txt
 }
