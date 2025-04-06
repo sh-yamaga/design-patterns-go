581d60ec4e86a3a954084667da49de1096a8679a
shu.yamaga <s.yamaga.0318@gmail.com>
2025-04-06 05:23:30 +0000

    [update]#observer_add statisticDisplay

--- cmd/obserber/main.go
+++ cmd/obserber/main.go
@@ -10,19 +10,43 @@ import (
 func main() {
 	weather := &observer.Weather{}
 
-	// create display(Observer)
+	// create displays(Observer)
 	currentDisplay := &display.CurrentDisplay{}
+	statisticsDisplay := &display.StatisticsDisplay{}
 
 	// add Observers
 	weather.AddObserver(currentDisplay)
+	weather.AddObserver(statisticsDisplay)
 
-	fmt.Println("=== Initial Measuremant ===")
+	fmt.Println("=== Initial Measuremants ===")
 	weather.SetMeasurements(25.0, 65.5, 1013.1)
 	// Output:
 	// === Initial Measuremant ===
 	// ▼ Current Weather
-	//   Temp :        25.0℃
-	//   Humid:        65.5%
-	//   Press:        1013.1hPa
+	// 	Temp :        25.0℃
+	// 	Humid:        65.5%
+	// 	Press:        1013.1hPa
+	// ---------------------------
+	// ▼ Statistics Weather
+	// 	Data Count:  1
+	// 	Avg Temp :   25.0℃
+	// 	Avg Humid:   65.5%
+	// 	Avg Press:   1013.1hPa
+	// ---------------------------
+
+	fmt.Println("=== 2nd Measuremants ===")
+	weather.SetMeasurements(22.8, 70.1, 1007.8)
+	// Output:
+	// === 2nd Measuremants ===
+	// ▼ Current Weather
+	//   Temp :        22.8℃
+	//   Humid:        70.1%
+	//   Press:        1007.8hPa
+	// ---------------------------
+	// ▼ Statistics Weather
+	//   Data Count:  2
+	//   Avg Temp :   23.9℃
+	//   Avg Humid:   67.8%
+	//   Avg Press:   1010.5hPa
 	// ---------------------------
 }
--- internal/patterns/observer/display/statistic.go
+++ internal/patterns/observer/display/statistic.go
@@ -1,4 +1,40 @@
 package display
 
+import (
+	"fmt"
+)
+
 type StatisticsDisplay struct {
+	tempRecord     []float64
+	humidRecord    []float64
+	pressureRecord []float64
+}
+
+func (sd *StatisticsDisplay) Update(temp, humidity, pressure float64) {
+	sd.tempRecord = append(sd.tempRecord, temp)
+	sd.humidRecord = append(sd.humidRecord, humidity)
+	sd.pressureRecord = append(sd.pressureRecord, pressure)
+	sd.Display()
+}
+
+func (sd *StatisticsDisplay) Display() {
+	averageTemp := getAverageMeasurement(sd.tempRecord)
+	averageHumid := getAverageMeasurement(sd.humidRecord)
+	averagePress := getAverageMeasurement(sd.pressureRecord)
+
+	fmt.Println("▼ Statistics Weather")
+	fmt.Printf("  Data Count:  %d\n", len(sd.tempRecord))
+	fmt.Printf("  Avg Temp :   %.1f℃\n", averageTemp)
+	fmt.Printf("  Avg Humid:   %.1f%%\n", averageHumid)
+	fmt.Printf("  Avg Press:   %.1fhPa\n", averagePress)
+	fmt.Println("---------------------------")
+}
+
+func getAverageMeasurement(records []float64) float64 {
+	var sum float64
+	for _, v := range records {
+		sum += v
+	}
+
+	return sum / float64(len(records))
 }
