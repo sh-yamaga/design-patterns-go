06904d2805bcf9d7ce6b048c1d67952b0068c6eb
shu.yamaga <s.yamaga.0318@gmail.com>
2025-04-09 08:48:24 +0000

    [update]#proxy_add output comment

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -14,14 +14,27 @@ func main() {
 	fmt.Println("=== 1st Query ===")
 	result, _ := proxyDB.Query("SELECT * FROM users;")
 	fmt.Println("Result:", result)
+	// Output:
+	// === 1st Query ===
+	// RealDatabase[Proxy Sample DB] executing query: SELECT * FROM users;
+	// Querying RealDatabase...
+	// Result: RealDatabaseResult
 
 	// 2nd Query
 	fmt.Println("=== 2nd Query ===")
 	result, _ = proxyDB.Query("SELECT * FROM users;")
 	fmt.Println("Result:", result)
+	// Output:
+	// === 2nd Query ===
+	// Proxy: using cache.
+	// Result: RealDatabaseResult
 
 	// 3rd Query
 	fmt.Println("=== 3rd Query ===")
 	result, _ = proxyDB.Query("SELECT * FROM users;")
 	fmt.Println("Result:", result)
+	// Output:
+	// === 3rd Query ===
+	// Proxy: using cache.
+	// Result: RealDatabaseResult
 }
