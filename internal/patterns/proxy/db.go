ddd06875fc8795ff313e0142b28f069139343e7b
shu.yamaga <s.yamaga.0318@gmail.com>
2025-04-09 08:33:27 +0000

    [update]#proxy_create db sample program

---
+++ internal/patterns/proxy/db.go
@@ -0,0 +1,32 @@
+package proxy
+
+import (
+	"fmt"
+	"time"
+)
+
+type Database interface {
+	Query(query string) (string, error)
+}
+
+// RealDatabase represents a concrete implementation of the Database interface
+type RealDatabase struct {
+	Name string
+}
+
+// Query executes the given query and returns a simulated result
+func (db *RealDatabase) Query(query string) (string, error) {
+	fmt.Printf("RealDatabase[%s] executing query: %s\n", db.Name, query)
+
+	// Simulate a heavy task with a sleep
+	fmt.Print("Sleeping")
+	for i := 0; i < 3; i++ {
+		fmt.Print(".")
+		time.Sleep(1 * time.Second)
+	}
+
+	// Simulated result
+	result := "RealDatabaseResult"
+
+	return result, nil
+}
---
+++ internal/patterns/proxy/proxy.go
@@ -0,0 +1,33 @@
+package proxy
+
+import "fmt"
+
+// ProxyDatabase is a struct that acts as a proxy to the RealDatabase
+type ProxyDatabase struct {
+	realDatabase *RealDatabase
+	lastQuery    string
+	lastResult   string
+	cacheEnabled bool
+}
+
+// Query executes the given query, incorporating caching mechanisms
+func (pdb *ProxyDatabase) Query(query string) (string, error) {
+	// Use cache if it is enabled and the query is the same as the last one.
+	if pdb.cacheEnabled && query == pdb.lastQuery && pdb.lastResult != "" {
+		fmt.Println("Proxy: using cache.")
+
+		return pdb.lastResult, nil
+	}
+
+	// Querying the RealDatabase
+	result, err := pdb.realDatabase.Query(query)
+	if err != nil {
+		return "", err
+	}
+
+	// Cache the result
+	pdb.lastQuery = query
+	pdb.lastResult = result
+
+	return result, nil
+}
